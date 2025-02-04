cfb932037b410f2e0d4be954740fd2193e84286d
KhiemAM <khiem2018khiem@gmail.com>
2025-02-04 18:39:19 +0700

    connection MongoDB Atlas

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,29 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+const MONGODB_URI = 'mongodb+srv://khiemam:khiemgold09022003*@cluster0.o6c52.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const DATABASE_NAME = 'trello_db'
+
+let trelloDatabaseInstance = null
+
+const mongoClientInstane = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONECT_DB = async () => {
+  await mongoClientInstane.connect()
+
+  trelloDatabaseInstance = mongoClientInstane.db(DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to database first')
+  return trelloDatabaseInstance
+}
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstane.close()
+}
--- src/server.js
+++ src/server.js
@@ -1,32 +1,37 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { CONECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
+import exitHook from 'async-exit-hook'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  const hostname = 'localhost'
+  const port = 8017
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', (req, res) => {
+    res.end('<h1>Hello World 2!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello KAM Dev, I am running at http://${hostname}:${port}/`)
+  })
+
+  exitHook(() => {
+    console.log('Exiting with exitHook')
+    CLOSE_DB()
+    console.log('Exiting with CLOSE_DB')
+  })
+}
+
+(async () => {
+  try {
+    await CONECT_DB()
+    console.log('Connected to database successfully')
+    START_SERVER()
+  } catch (error) {
+    console.error('Error connecting to database:', error)
+    process.exit(0)
+  }
+})()
+
