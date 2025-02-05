773d7e580dfe3807fdcfd879cc17788932579da8
KhiemAM <khiem2018khiem@gmail.com>
2025-02-05 12:55:21 +0700

    using Joi Validation

---
+++ src/validations/boardValidation.js
@@ -0,0 +1,23 @@
+import Joi from 'joi'
+import { StatusCodes } from 'http-status-codes'
+
+const createNew = async (req, res) => {
+  const correctCondition = Joi.object({
+    title: Joi.string().required().min(3).max(50).trim().strict(),
+    description: Joi.string().required().min(3).max(256).trim().strict()
+  })
+
+  try {
+    await correctCondition.validateAsync(req.body, { abortEarly: false })
+
+    res.status(StatusCodes.CREATED).json({ message: 'POST: API create new board' })
+  } catch (error) {
+    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+      errors: new Error(error).message
+    })
+  }
+}
+
+export const boardValidation = {
+  createNew
+}
