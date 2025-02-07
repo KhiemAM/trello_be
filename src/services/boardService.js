e352d57052f0d775d5a4575094e9d75902574201
KhiemAM <khiem2018khiem@gmail.com>
2025-02-07 22:28:43 +0700

    api get board details

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,4 +1,7 @@
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (reqBody) => {
   try {
@@ -7,12 +10,29 @@ const createNew = async (reqBody) => {
       slug: slugify(reqBody.title)
     }
 
-    return newBoard
+    const createdBoard = await boardModel.createNew(newBoard)
+
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+
+    return getNewBoard
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const getDetails = async (boardId) => {
+  try {
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found')
+    }
+    return board
   } catch (error) {
     throw new Error(error)
   }
 }
 
 export const boardService = {
-  createNew
+  createNew,
+  getDetails
 }
