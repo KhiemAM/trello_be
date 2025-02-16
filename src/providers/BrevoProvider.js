fb24335cc38efaa0389acf6dbe5d1e99d5cef95a
KhiemAM <khiem2018khiem@gmail.com>
2025-02-16 13:54:44 +0700

    email verify

---
+++ src/providers/BrevoProvider.js
@@ -0,0 +1,20 @@
+const SibApiV35dk = require('@getbrevo/brevo')
+import { env } from '~/config/environment'
+
+let apiInstance = new SibApiV35dk.TransactionalEmailsApi()
+let apiKey = apiInstance.authentications['apiKey']
+apiKey.apiKey = env.BREVO_API_KEY
+
+const sendEmail = async (recipientEmail, customSubject, htmlContent) => {
+  let sendSmtpEmail = new SibApiV35dk.SendSmtpEmail()
+  sendSmtpEmail.sender = { email: env.ADMIN_EMAIL_ADDRESS, name: env.ADMIN_EMAIL_NAME }
+  sendSmtpEmail.to = [{ email: recipientEmail }]
+  sendSmtpEmail.subject = customSubject
+  sendSmtpEmail.htmlContent = htmlContent
+
+  return apiInstance.sendTransacEmail(sendSmtpEmail)
+}
+
+export const BrevoProvider = {
+  sendEmail
+}
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,6 +4,8 @@ import { StatusCodes } from 'http-status-codes'
 import bcryptjs from 'bcryptjs'
 import { v4 as uuidv4 } from 'uuid'
 import { pickUser } from '~/utils/formatters'
+import { WEBSITE_DOMAIN } from '~/utils/constants'
+import { BrevoProvider } from '~/providers/BrevoProvider'
 
 const createNew = async (reqBody) => {
   try {
@@ -24,6 +26,18 @@ const createNew = async (reqBody) => {
 
     const createdUser = await userModel.createNew(newUser)
     const getNewuser = await userModel.findOneById(createdUser.insertedId)
+
+    const verificationLink = `${WEBSITE_DOMAIN}/account/verification?email=${getNewuser.email}&token=${getNewuser.verifyToken}`
+    const customSubject = 'Trello - Verify your email address'
+    const htmlContent = `
+      <h3>Hi ${getNewuser.displayName},</h3>
+      <p>Thank you for signing up for Trello! You're almost ready to start using your account. Please click the link below to verify your email address.</p>
+      <a href="${verificationLink}">Verify your email address</a>
+      <p>If you didn't sign up for Trello, you can safely ignore this email.</p>
+      <p>Thanks,</p>
+      <p>The Trello team</p>
+    `
+    await BrevoProvider.sendEmail(getNewuser.email, customSubject, htmlContent)
 
     return pickUser(getNewuser)
   } catch (error) {
